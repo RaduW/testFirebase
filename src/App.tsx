d96213a4a227d13a86bacfb7bd2d461a7d3ab619
Radu Woinaroski <radu@sentry.io>
2021-05-25 20:03:50 +0200

    before adding firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let elements = [];
+    for (let i = 0; i < 50; ++i) {
+        elements.push(PictureFrame())
+    }
+    return (
+        <div className="App">
+            <div className="Spacer"></div>
+            <header className="App-header">
+                {elements}
+            </header>
+            <div className="Spacer"></div>
+        </div>
+    );
+}
+
+function PictureFrame() {
+    return (
+        <div className="PictureWrapper">
+            <div className="PictureFrame">
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
